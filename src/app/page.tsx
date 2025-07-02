11b2501995cd97cf6f70c5d22f65dfa1e6b14b13
renelps <renanlps616@gmail.com>
2025-07-02 07:43:00 -0300

    Implement global CSS and base layout structure for consistent styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
 import Image from "next/image";
+import logo from "../../public/logo.png";
+import Link from "next/link";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+export default function Home(){
 
-export default function Home() {
+  async function handleLogin(formData: FormData){
+    "use server"
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if(email === "" || password === "") return;
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password
+      })
+
+      if(!response.data.token) return;
+
+      console.log(response)
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+      const cookiesStore = await cookies();
+
+      cookiesStore.set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+
+      })
+    }
+    catch(err){
+      console.log(err)
+      return;
+    }
+
+    redirect("/dashboard")
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex items-center justify-center h-screen">
+      <div
+        className="w-full max-w-xl pb-20 px-10 md:px-18 shadow-none md:shadow-[0_6px_25px_rgba(111,78,55,0.5)] flex items-center flex-col rounded-xl">
+        <div>
+          <Image
+            src={logo}
+            alt="logo"
+            quality={100}
+            priority={true}
+            width={350}
+            height={350}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+        <section className="w-full">
+          <form 
+            action={handleLogin}  
+            className="flex flex-col gap-4 w-full">
+            <input 
+              type="email"
+              name="email" 
+              required
+              className="border-1 border-[#5D4037] py-3 px-2 rounded-sm outline-none"
+              placeholder="Digite seu email"
+            />
+            <input 
+              type="password" 
+              name="password"
+              required
+              className="border-1 border-[#5D4037] py-3 px-2 rounded-sm outline-none" 
+              placeholder="Digite sua senha"
+            />
+
+            <button 
+              type="submit" 
+              className="mt-10 border-1 border-[#5D4037] py-3 hover:border-slate-600 duration-300"
+            >
+              Acessar
+            </button>
+          </form>
+
+          <div className="text-center mt-7">
+            <Link
+              href="/signup"
+              className="border-b-1 text-slate-300"
+            >
+              Não tem conta? Crie a sua agora mesmo!
+            </Link>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
